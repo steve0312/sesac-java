14924f0b90a8ceb191727dc73b10956068722f76
sangho <steve0312@naver.com>
2024-11-07 01:14:06 +0900

    1105 실습 : Vite를 통한 React 환경 세팅 및 작동 테스트

---
+++ React/my-app/src/App.jsx
@@ -0,0 +1,7 @@
+import Article from './Article';
+
+function App() {
+  return <Article></Article>;
+}
+
+export default App;
---
+++ React/my-app/src/Article.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Article() {
+  return (
+    <div>
+      <h1>제목입니다</h1>
+      <p>내용입니다</p>
+    </div>
+  );
+}
