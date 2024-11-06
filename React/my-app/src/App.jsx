7e1ff6370b26fa0d03d59eaa14e2258ff10cb274
sangho <steve0312@naver.com>
2024-11-07 01:22:53 +0900

    1105 실습 : Welcome 컴포넌트 추가

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -1,13 +1,15 @@
-import Article from './Article';
-import Todos from './Todos';
-import Component from './Component';
+// import Article from './Article';
+// import Todos from './Todos';
+// import Component from './Component';
+import Welcome from './Welcome';
 
 function App() {
   return (
     <>
-      <Article></Article>
+      {/* <Article></Article>
       <Todos></Todos>
-      <Component></Component>
+      <Component></Component> */}
+      <Welcome name={'안녕하세요'}></Welcome>
     </>
   );
 }
