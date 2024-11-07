d23078248f736878f679daeeccfbd195f160533a
sangho <steve0312@naver.com>
2024-11-07 23:42:18 +0900

    1106 실습 : Button 컴포넌트에 Children 적용

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -4,6 +4,7 @@
 // import Welcome from './Welcome';
 // import Card from './Card';
 import Button from './Button';
+import ButtonChildren from './ButtonChildren';
 
 function App() {
   return (
@@ -13,10 +14,14 @@ function App() {
       <Component></Component> */}
       {/* <Welcome name={'안녕하세요'}></Welcome> */}
       {/* <Card title="제목123" content="내용" width="200px" height="200px"></Card> */}
-      <Button backgroundColor="blue" text="확인"></Button>
+      {/* <Button backgroundColor="blue" text="확인"></Button>
       <Button backgroundColor="red" text="취소"></Button>
       <Button backgroundColor="gray" text="보류"></Button>
-      <Button backgroundColor="pink" text="1억년"></Button>
+      <Button backgroundColor="pink" text="1억년"></Button> */}
+      <ButtonChildren backgroundColor="blue">확인</ButtonChildren>
+      <ButtonChildren backgroundColor="red">취소</ButtonChildren>
+      <ButtonChildren backgroundColor="gray">보류</ButtonChildren>
+      <ButtonChildren backgroundColor="pink">1억년</ButtonChildren>
     </>
   );
 }
---
+++ React/my-app/src/ButtonChildren.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Button({ backgroundColor, color = 'white', children }) {
+  return <button style={{ backgroundColor, color }}>{children}</button>;
+}
