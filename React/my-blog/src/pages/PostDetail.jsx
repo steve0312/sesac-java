2162005146131d9bad32cb6da387fe89bcad6c7f
sangho <steve0312@naver.com>
2024-11-12 23:10:40 +0900

    1111 실습 : PostDetail 컴포넌트 수정- useLocation

--- React/my-blog/src/pages/PostDetail.jsx
+++ React/my-blog/src/pages/PostDetail.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 
 export default function PostDetail() {
   const { postId } = useParams();
 
+  // useLocation을 통해 PostDetail에서 받은 post를 활용
+  const location = useLocation();
+  const { post } = location.state;
+
   return (
-    <div>{postId}번째 게시글</div>
-  )
+    <div>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
 }
