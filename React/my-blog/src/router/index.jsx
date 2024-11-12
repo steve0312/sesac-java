31880fa11ffa6ea12529782eb33002f0342d077e
sangho <steve0312@naver.com>
2024-11-12 23:13:37 +0900

    1111 실습 : NotFound 컴포넌트 추가 / index 컴포넌트 수정

--- React/my-blog/src/router/index.jsx
+++ React/my-blog/src/router/index.jsx
@@ -3,12 +3,15 @@ import Home from '../pages/Home';
 import PostList from '../pages/PostList';
 import RootLayout from '../RootLayout';
 import PostDetail from '../pages/PostDetail';
+import NotFound from '../pages/NotFound';
 
 // createBrowserRouter() 를 통해 URL 경로와 컴포넌트를 매핑시킬 수 있다.
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    // 존재하지 않는 URL에 접근하는 등 에러가 난 경우, NotFound 컴포넌트 작동
+    errorElement: <NotFound />,
     // children을 통해 중첩된 라우트를 지정
     children: [
       {
