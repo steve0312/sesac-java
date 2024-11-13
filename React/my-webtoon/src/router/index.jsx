31ed0fad8e0b2cb687f88cf67413eb60a951822f
sangho <steve0312@naver.com>
2024-11-13 13:59:20 +0900

    1112 실습 : index 컴포넌트 구조 리팩토링 / Home 컴포넌트 수정

--- React/my-webtoon/src/router/index.jsx
+++ React/my-webtoon/src/router/index.jsx
@@ -6,6 +6,8 @@ import RootLayout from '../layouts/RootLayout';
 import WebtoonCategory from '../pages/WebtoonCategory';
 import NovelCategory from '../pages/NovelCategory';
 
+// 아래의 코드가 문제에서 의도한 코드가 맞다. 어차피 경로를 "/"로 설정하면 되니까 굳이 WebtoonLayout과 NovleLayout의 자식으로 가지는 것보다 최상단으로 뺴면 의도한대로 동작함
+// 리팩토링한 코드이고 이전 코드는 아래에 주석처리 해둠
 const router = createBrowserRouter([
   {
     path: '/',
@@ -20,8 +22,10 @@ const router = createBrowserRouter([
         element: <WebtoonLayout />,
         children: [
           {
+            // path: ":dayId",
             path: '/webtoon/:dayId',
             element: <WebtoonCategory />,
+            // loader
           },
         ],
       },
@@ -30,8 +34,10 @@ const router = createBrowserRouter([
         element: <NovelLayout />,
         children: [
           {
+            // path: ":genreId",
             path: '/novel/:genreId',
             element: <NovelCategory />,
+            // loader
           },
         ],
       },
