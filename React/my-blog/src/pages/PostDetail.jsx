7fdee26ebdf73fe74ec14629de6d4b2663d5ecd7
sangho <steve0312@naver.com>
2024-11-16 14:27:48 +0900

    1113 실습 : useSelector를 통해 게시글 데이터를 가져와서 사용, 화면에 한 번 렌더링이 되고나서 useEffect와 useState를 통해 화면에 게시글 상세 내용 출력

--- React/my-blog/src/pages/PostDetail.jsx
+++ React/my-blog/src/pages/PostDetail.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 export default function PostDetail() {
   const { postId } = useParams();
+  // useSelector를 통해 store에 정의된 posts를 가져옴
+  const posts = useSelector((state) => state.posts);
 
-  // useLocation을 통해 PostDetail에서 받은 post를 활용
-  const location = useLocation();
-  const { post } = location.state;
+  // 위에서 이렇게 어떤 게시글(Post)을 가져올건지 미리 정하면 렌더링 때 정상 작동함
+  // const post = posts.find((post) => post.id === parseInt(postId));
+  const [post, setPost] = useState();
+
+  useEffect(() => {
+    setPost(posts.find((post) => post.id === parseInt(postId)));
+  }, []);
 
   return (
     <div>
-      <h3>{post.title}</h3>
-      <p>{post.content}</p>
+      detail
+      <h3>{post?.title}</h3>
+      <p>{post?.content}</p>
     </div>
   );
 }
