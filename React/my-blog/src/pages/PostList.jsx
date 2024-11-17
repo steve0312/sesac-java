eedc8385b0c5e2002e70bc20ec405cc612e9310b
sangho <steve0312@naver.com>
2024-11-18 01:39:27 +0900

    1115 실습 : 서버(json-server)로 게시글 목록을 요청(axios.get)해서 응답받는 작업 진행

--- React/my-blog/src/pages/PostList.jsx
+++ React/my-blog/src/pages/PostList.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function PostList() {
   // useSelector를 통해 store에 정의된 posts를 가져옴
-  const posts = useSelector((state) => state.posts);
+  // const posts = useSelector((state) => state.posts);
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPost() {
+      const url = 'http://localhost:3000/posts';
+      const response = await axios.get(url);
+      // const response = await axios({url: url});
+
+      const data = response.data;
+      setPosts(data);
+    }
+    fetchPost();
+  }, []);
 
   return (
     <div>
