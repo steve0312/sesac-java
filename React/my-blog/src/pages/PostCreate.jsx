7cf6bceb9f87028c1098fc13f15e2f5c4f639224
sangho <steve0312@naver.com>
2024-11-17 23:19:21 +0900

    1114 실습 : my-blog에 게시글 생성 페이지 구현

--- React/my-blog/src/pages/PostCreate.jsx
+++ React/my-blog/src/pages/PostCreate.jsx
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function PostCreate() {
-  return <div>PostCreate</div>;
+  const [formData, setFormData] = useState({ title: '', content: '' });
+
+  function handleChange(e) {
+    const inputValue = e.target.value;
+    const key = e.target.name;
+    // input의 value로 state를 변경한다.
+    setFormData({
+      ...formData,
+      // key를 변수로서 활용 가능
+      [key]: inputValue,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <h3>PostCreate</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">제목 : </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          // input 태그에 입력이 되었을 때 state를 변경한다.
+          onChange={handleChange}
+        ></input>
+
+        <label>
+          내용 :
+          <textarea name="content" id="" onChange={handleChange}></textarea>
+        </label>
+
+        <button>제출</button>
+      </form>
+    </>
+  );
 }
