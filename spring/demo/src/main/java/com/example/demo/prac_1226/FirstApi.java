ce12d10226fc38da3ff4721410695e4fd2fdedcc
sangho <steve0312@naver.com>
2024-12-26 23:28:49 +0900

    1226 실습 : Spring 간단한 게시판 구현 실습(1)
    12/26 수업 때 '간단한 게시판 구현' 강의자료에 있던 실습 내용 구현함
    
    === DB 없이 List에서 게시글(post) CRUD 기능 구현 ===
    1. PostApi
    => @RestController, @GetMapping 활용
    
    2. PostApi2
    => PostApi에서 Restful한 URL 형태로 설정(@PostMapping, @GetMapping, @PutMapping, @DeleteMapping 활용)
    
    3. PostApi3
    => PostApi2에서 공통 URL 경로 설정(@RequestMapping 활용)

---
+++ spring/demo/src/main/java/com/example/demo/prac_1226/FirstApi.java
@@ -0,0 +1,18 @@
+package com.example.demo.prac_1226;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstApi {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "hello spring";
+    }
+
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "Hello User";
+    }
+}
---
+++ spring/demo/src/main/java/com/example/demo/prac_1226/PostApi2.java
@@ -0,0 +1,91 @@
+package com.example.demo.prac_1226;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 요청을 받고 처리
+@RestController
+public class PostApi2 {
+    // Post 클래스의 인스턴스, 즉 게시글을 넣음
+    List<Post> posts = new ArrayList<>();   // DB를 사용하지 않고 게시글을 하는 방식이라서 List 사용
+    private Long id = 0L;   // 게시글 생성에서 id 생성을 위한 장치
+
+    {
+        // 초기화 블록
+        // 인스턴스가 생성되었을 때 한 번 실행된다.
+        // 초기값을 넣을 때 사용된다.
+        posts.add(new Post(++id, "초기 제목", "초기 내용"));
+    }
+
+    // create에 대한 동작들
+    // post / 내용 / url
+//    @GetMapping("/v2/posts/create")
+    @PostMapping("/v2/posts")
+    public Post createPost() {
+        Post post = new Post(++id, "제목", "내용");
+        posts.add(post);
+
+        return post;
+    }
+
+    // read- 전체 조회
+    // posts / get
+//    @GetMapping("/v2/posts")
+    @GetMapping("/v2/posts")
+    public List<Post> readPosts() {
+        return posts;
+    }
+
+    // read- 단일(특정 게시글) 조회
+    // posts/{id}
+    // {id} 는 리액트에서 :id (동적 라우터)와 같은 의미
+    @GetMapping("/v2/posts/{id}")
+    public Post readPostById(@PathVariable Long id) {
+        // posts에서 post를 가져오자
+        for (Post post : posts) {
+            if(post.getId().equals(id)) {
+                return post;
+            }
+        }
+
+        // id가 일치하는 게시글이 없는 경우
+        return null;
+    }
+
+    // update
+    // 변경 내용 / id / url
+//    @GetMapping("/v2/posts/{id}/update")
+    @PutMapping("/v2/posts/{id}")
+    public Post updatePost(@PathVariable Long id) {
+        for (Post post : posts) {
+            if(post.getId().equals(id)) {
+                post.setTitle("수정된 제목");
+                post.setContent("수정된 내용");
+
+                return post;
+            }
+        }
+
+        return null;
+    }
+
+    // delete
+    // id / url
+    @DeleteMapping("/v2/posts/{id}")
+    public Post deletePost(@PathVariable Long id) {
+        Post removedPost = null;
+
+        for (Post post : posts) {
+            if(post.getId().equals(id)) {
+                removedPost = post;
+                break;
+            }
+        }
+
+        posts.remove(removedPost);
+
+        return null;
+    }
+}
