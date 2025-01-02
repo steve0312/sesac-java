2865b525455e931722f994d42ca26b2637599465
sangho <steve0312@naver.com>
2025-01-02 23:44:05 +0900

    0102 실습 : ResponseEntity 실습
    01/02 수업 때 'ResponseEntity' 강의자료에 있던 실습 내용 구현함
    
    === ResponseEntity와 API 응답 구조화 활용 ===
    ResponseEntity와 message, code, data 응답 구조화
    => HTTP 응답을 상세하게 제어

--- spring/demo/src/main/java/com/example/demo/prac_0102/myjpasitev4/PostControllerV4.java
+++ spring/demo/src/main/java/com/example/demo/prac_0102/myjpasitev4/PostControllerV4.java
@@ -6,6 +6,7 @@ import com.example.demo.prac_0102.myjpasitev4.dto.PostResponseDto;
 import com.example.demo.prac_0102.myjpasitev4.dto.PostUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,30 +22,58 @@ public class PostControllerV4 {
     // Post method / url / data
     // 클라이언트가 요청한 데이터를 RequestDTO를 통해 서버로 전달
     @PostMapping
-    public PostResponseDto createPost(@RequestBody PostCreateRequestDto requestDto) {
-        return postServiceV4.createPost(requestDto);
+    public ResponseEntity<ApiResponse<PostResponseDto>> createPost(@RequestBody PostCreateRequestDto reqeustDto){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        ApiResponse.ok(
+                                "게시글이 정상적으로 작성되었습니다",
+                                "CREATED",
+                                postServiceV4.createPost(reqeustDto)
+                        )
+                );
+
     }
 
     // Get method / url / 전체 data
     @GetMapping
-    public List<PostListResponseDto> readPosts() {
-        return postServiceV4.readPosts();
+    public ResponseEntity<ApiResponse<List<PostListResponseDto>>> readPosts(){
+        List<PostListResponseDto> data = postServiceV4.readPosts();
+        ApiResponse<List<PostListResponseDto>> response = ApiResponse.ok(data);
+
+        return ResponseEntity.ok(response);
+
     }
 
     @GetMapping("/{id}")
-    public PostResponseDto readPostById(@PathVariable Long id) {
-        return postServiceV4.readPostById(id);
+    public ResponseEntity<ApiResponse<PostResponseDto>> readPostById(@PathVariable Long id){
+
+        return ResponseEntity.ok(ApiResponse.ok(postServiceV4.readPostById(id)));
     }
 
     // Put method / id / dto data
     @PutMapping("/{id}")
-    public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostUpdateRequestDto postUpdateRequestDto) {
-        return postServiceV4.updatePost(id, postUpdateRequestDto);
+    public ResponseEntity<ApiResponse<PostResponseDto>> updatePost(@PathVariable Long id, @RequestBody PostUpdateRequestDto requestDto){
+        return ResponseEntity.ok(ApiResponse.ok(postServiceV4.updatePost(id, requestDto)));
     }
 
+//    @DeleteMapping("/{id}")
+////    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public ResponseEntity<Void> deletePost(@PathVariable Long id){
+//        postServiceV4.deletePost(id);
+//        return  ResponseEntity.noContent().build();
+//    }
+
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletePost(@PathVariable Long id) {
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<ApiResponse<Void>> deletePost(@PathVariable Long id){
         postServiceV4.deletePost(id);
+        return ResponseEntity.ok(
+                ApiResponse.ok(
+                        "게시글이 정상적으로 삭제되었습니다",
+                        "DELETED",
+                        null
+                )
+        );
     }
 }
